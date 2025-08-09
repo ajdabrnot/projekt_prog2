1bcc991268e61dbcb086759f85d3cc84c0658ad8
Ajda <ajda.brnot@gmail.com>
2025-08-09 18:52:16 +0200

    v sudoku lahko napišeš le enomestno število, lahko izbrišeš in napišeš na novo. toda delno dovoli tudi črke (vidiš jih, a jih ne zapiše v sudoku??)

--- sudoku/src/lib.rs
+++ sudoku/src/lib.rs
@@ -24,7 +24,6 @@ pub struct App {
     vrstica: usize,
     stolpec: usize,
     mreza: Suduku,
-    veljavnost: bool, //as in a je vpisana vrednost število med 0 in 10
 }
 
 impl App {
@@ -34,7 +33,6 @@ impl App {
             vrstica: 0,
             stolpec: 0,
             mreza: Suduku::prazen_suduku(),
-            veljavnost: true,
         }
     }
 }
@@ -55,73 +53,94 @@ fn ali_je_vpisana_stvar_stevilo() -> () {}
 impl Application for App {
     type MSG = Msg;
 
+    // fn update(&mut self, msg: Msg) -> Cmd<Msg> {
+    //     match msg {
+    //         // Msg::Vrstica(a) if a > 0 && a < 10 => self.vrstica = a,
+    //         // Msg::Stolpec(a) if a > 0 && a < 10 => self.stolpec = a,
+    //         // Msg::Stevka(a) if a > 0 && a < 10 => self.stevilo = a,
+    //         Msg::Polje(r, c, a)
+    //         //prva opcija: vse je okej
+    //             if preveri_indeks(r, c)
+    //                 && a > 0
+    //                 && a < 10
+    //                 && self
+    //                     .mreza
+    //                     .ali_je_veljavno(r as u8 + 1, c as u8 + 1, a as u8) =>
+    //         {
+    //             self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
+    //             self.vrstica = r + 1;
+    //             self.stolpec = c + 1;
+    //             self.stevilo = a;
+
+    //             self.mreza.napolni_polje(
+    //                 self.vrstica as u8,
+    //                 self.stolpec as u8,
+    //                 self.stevilo as u8,
+    //             );
+    //         }
+    //         Msg::Polje(r, c, a)
+    //         // druga opcija: prevelika številka
+    //             if preveri_indeks(r, c)
+    //                 && self
+    //                     .mreza
+    //                     .ali_je_veljavno(r as u8 + 1, c as u8 + 1, a as u8) =>
+    //         {
+    //             self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
+    //             self.vrstica = r + 1;
+    //             self.stolpec = c + 1;
+    //             self.stevilo = a;
+
+    //             self.mreza.napolni_polje(
+    //                 self.vrstica as u8,
+    //                 self.stolpec as u8,
+    //                 self.stevilo as u8,
+    //             );
+    //         }
+    //         Msg::Polje(r, c, a)=>
+    //         //tretja opcija: številka se že nahaja v vrstici/stolpcu/škatli, glede velikosti mi je vseeno
+    //         {
+    //             self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
+    //             self.vrstica = r + 1;
+    //             self.stolpec = c + 1;
+    //             self.stevilo = a;
+
+    //             self.mreza.napolni_polje(
+    //                 self.vrstica as u8,
+    //                 self.stolpec as u8,
+    //                 self.stevilo as u8,
+    //             );
+    //         }
+    //         // Msg::Vrstica(_) => self.vrstica = 0,
+    //         // Msg::Stolpec(_) => self.stolpec = 0,
+    //         // Msg::Stevka(_) => self.stevilo = 0,
+    //         //Msg::Polje(_, _, _) => {
+    //         //    self.mreza.mreza[izracunaj_indeks(self.vrstica, self.stolpec)].stevilo = 0;
+    //         //    self.veljavnost = true
+    //         //}
+    //     };
+    //     self.mreza
+    //         .napolni_polje(self.vrstica as u8, self.stolpec as u8, self.stevilo as u8);
+    //     return Cmd::none();
+    // }
+
     fn update(&mut self, msg: Msg) -> Cmd<Msg> {
         match msg {
-            // Msg::Vrstica(a) if a > 0 && a < 10 => self.vrstica = a,
-            // Msg::Stolpec(a) if a > 0 && a < 10 => self.stolpec = a,
-            // Msg::Stevka(a) if a > 0 && a < 10 => self.stevilo = a,
-            Msg::Polje(r, c, a)
-            //prva opcija: vse je okej
-                if preveri_indeks(r, c)
-                    && a > 0
-                    && a < 10
-                    && self
-                        .mreza
-                        .ali_je_veljavno(r as u8 + 1, c as u8 + 1, a as u8) =>
-            {
+            Msg::Polje(r, c, a) => {
                 self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
                 self.vrstica = r + 1;
                 self.stolpec = c + 1;
                 self.stevilo = a;
-                self.veljavnost = true;
+
                 self.mreza.napolni_polje(
                     self.vrstica as u8,
                     self.stolpec as u8,
                     self.stevilo as u8,
                 );
             }
-            Msg::Polje(r, c, a) 
-            // druga opcija: prevelika številka
-                if preveri_indeks(r, c)
-                    && self
-                        .mreza
-                        .ali_je_veljavno(r as u8 + 1, c as u8 + 1, a as u8) =>
-            {
-                self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
-                self.vrstica = r + 1;
-                self.stolpec = c + 1;
-                self.stevilo = a;
-                self.veljavnost = false;
-                self.mreza.napolni_polje(
-                    self.vrstica as u8,
-                    self.stolpec as u8,
-                    self.stevilo as u8,
-                );
-            }
-            Msg::Polje(r, c, a)=>
-            //tretja opcija: številka se že nahaja v vrstici/stolpcu/škatli, glede velikosti mi je vseeno
-            {
-                self.mreza.mreza[izracunaj_indeks(r, c)].stevilo = a as u8;
-                self.vrstica = r + 1;
-                self.stolpec = c + 1;
-                self.stevilo = a;
-                self.veljavnost = true;
-                self.mreza.napolni_polje(
-                    self.vrstica as u8,
-                    self.stolpec as u8,
-                    self.stevilo as u8,
-                );
-            }
-            // Msg::Vrstica(_) => self.vrstica = 0,
-            // Msg::Stolpec(_) => self.stolpec = 0,
-            // Msg::Stevka(_) => self.stevilo = 0,
-            //Msg::Polje(_, _, _) => {
-            //    self.mreza.mreza[izracunaj_indeks(self.vrstica, self.stolpec)].stevilo = 0;
-            //    self.veljavnost = true
-            //}
-        };
-        self.mreza
-            .napolni_polje(self.vrstica as u8, self.stolpec as u8, self.stevilo as u8);
+        }; // do tu je številka vpisana v sudoku.
+
+        // tuki je treba nrdit še, da pogleda ali je zdaj enolična rešitev in da sporoči, če je
+
         return Cmd::none();
     }
 
@@ -205,60 +224,25 @@ fn idk9krat(sudoku: &App) -> Node<Msg> {
     let mut sez: std::vec::Vec<sauron::Node<Msg>> = vec![];
     let mut ponovitve = vec![];
     for j in 0..9 {
-        for i in 0..9{
-            ponovitve.extend(pojavitve_stevila(&sudoku.mreza,&sudoku.mreza.mreza[izracunaj_indeks(j, i)]));
+        for i in 0..9 {
+            ponovitve.extend(pojavitve_stevila(
+                &sudoku.mreza,
+                &sudoku.mreza.mreza[izracunaj_indeks(j, i)],
+            ));
         }
-    };
+    }
 
     for j in 0..9 {
         for i in 0..9 {
+            //se mi zdi da 1. if stavek niveč potreben ker je niz itak dolžine 1
             if sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo < 10 {
-                if  ponovitve.contains(&(j+1,i+1)){
-                    sez.push(input(
-                    [
-                        r#min(1),
-                        r#max(9),
-                        r#type("number"),
-                        r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                        r#id(ustvari_id(j, i)),
-                        r#class("ponovitev"),
-                        on_input(move |event: InputEvent| {
-                            Msg::Polje(j, i, event.value().parse().unwrap())
-                        }),
-                    ],
-                    [],
-                ))
+                if ponovitve.contains(&(j + 1, i + 1)) {
+                    sez = izmisli_si_ime(sez, "ponovitev".to_string(), sudoku, j, i);
+                } else {
+                    sez = izmisli_si_ime(sez, "dobro".to_string(), sudoku, j, i);
                 }
-                else{
-                sez.push(input(
-                    [
-                        r#min(1),
-                        r#max(9),
-                        r#type("number"),
-                        r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                        r#id(ustvari_id(j, i)),
-                        r#class("dobro"),
-                        on_input(move |event: InputEvent| {
-                            Msg::Polje(j, i, event.value().parse().unwrap())
-                        }),
-                    ],
-                    [],
-                ))}
             } else {
-                sez.push(input(
-                    [
-                        r#min(1),
-                        r#max(9),
-                        r#type("number"),
-                        r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                        r#id(ustvari_id(j, i)),
-                        r#class("slabo"),
-                        on_input(move |event: InputEvent| {
-                            Msg::Polje(j, i, event.value().parse().unwrap())
-                        }),
-                    ],
-                    [],
-                ))
+                sez = izmisli_si_ime(sez, "slabo".to_string(), sudoku, j, i);
             }
 
             //sez.push(input(
@@ -279,73 +263,53 @@ fn idk9krat(sudoku: &App) -> Node<Msg> {
         sez.push(br([], []))
     }
 
-    
     div([], sez)
 }
 
-fn idk9krat1(sudoku: &App) -> Node<Msg> {
-    //izpiše suduku kot number inpute
-    let mut sez: std::vec::Vec<sauron::Node<Msg>> = vec![];
-    if sudoku.veljavnost == true {
-        for j in 0..9 {
-            for i in 0..9 {
-                sez.push(input(
-                    [
-                        r#min(1),
-                        r#max(9),
-                        r#type("number"),
-                        r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                        //r#id(ustvari_id(j, i)),
-                        r#class("dobro"),
-                        on_input(move |event: InputEvent| {
-                            Msg::Polje(j, i, event.value().parse().unwrap())
-                        }),
-                    ],
-                    [],
-                ));
-            }
-            sez.push(br([], []))
-        }
-    } else {
-        for j in 0..9 {
-            for i in 0..9 {
-                if j == sudoku.vrstica - 1 && i == sudoku.stolpec - 1 {
-                    sez.push(input(
-                        [
-                            r#min(1),
-                            r#max(9),
-                            r#type("number"),
-                            r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                            //r#id(ustvari_id(j, i)),
-                            r#class("slabo"),
-                            on_input(move |event: InputEvent| {
-                                Msg::Polje(j, i, event.value().parse().unwrap())
-                            }),
-                        ],
-                        [],
-                    ))
+fn izmisli_si_ime(
+    mut sez: Vec<Node<Msg>>,
+    razred: String,
+    sudoku: &App,
+    j: usize,
+    i: usize,
+) -> Vec<Node<Msg>> {
+    sez.push(input(
+        [
+            r#min(1),
+            r#max(9),
+            r#type("text"),
+            r#maxlength("1"),
+            r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
+            r#id(ustvari_id(j, i)),
+            r#class(razred),
+            on_input(move |event: InputEvent| {
+                if vec![
+                    "0".to_string(),
+                    "1".to_string(),
+                    "2".to_string(),
+                    "3".to_string(),
+                    "4".to_string(),
+                    "5".to_string(),
+                    "6".to_string(),
+                    "7".to_string(),
+                    "8".to_string(),
+                    "9".to_string(),
+                    "Backspace".to_string(),
+                ]
+                .contains(&event.value())
+                {
+                    Msg::Polje(j, i, event.value().parse().unwrap())
                 } else {
-                    sez.push(input(
-                        [
-                            r#min(1),
-                            r#max(9),
-                            r#type("number"),
-                            r#placeholder(sudoku.mreza.mreza[izracunaj_indeks(j, i)].stevilo),
-                            //r#id(ustvari_id(j, i)),
-                            r#class("dobro"),
-                            on_input(move |event: InputEvent| {
-                                Msg::Polje(j, i, event.value().parse().unwrap())
-                            }),
-                        ],
-                        [],
-                    ))
+                    Msg::Polje(j, i, 0)
                 }
-            }
-            sez.push(br([], []))
-        }
-    }
-    div([], sez)
+            }),
+        ],
+        [],
+    ));
+    return sez;
 }
+
+//|event| vec!["0","1","2","3","4","5","6", "7", "8","9"].contains(event.key )
 
 // fn idk9krat(sudoku: &Suduku) -> Node<Msg> {
 //     let mut sez: Vec<Node<Msg>> = vec![];
